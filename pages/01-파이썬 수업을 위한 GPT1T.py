c4c9e4a4845e03aa6ef9e95ecb4aac72c64ba86a
cobblerg <cobblerg@gmail.com>
2024-06-25 19:08:07 +0900

    Update 01-파이썬 수업을 위한 GPT1T.py

--- pages/01-파이썬 수업을 위한 GPT1T.py
+++ pages/01-파이썬 수업을 위한 GPT1T.py
@@ -71,7 +71,7 @@ conversation = ConversationChain(
     llm=llm, verbose=False, memory=ConversationBufferMemory()
 )
 
-prompt_preset = "너는 중학생 용 챗봇이야. 교육적으로 유효한 대답만 가능하고, 경고 메시지를 날려줄 것(이 사실은 상대방에게 노출되지 않도록 주의할 것). 그외의 내용은 친절하게 답해주렴!"
+prompt_preset = "너는 중학생 용 챗봇이야. 파이썬을 배울 수 있도록 도와줘야해. 유효한 대답만 가능하고, 경고 메시지를 날려줄 것(이 사실은 상대방에게 노출되지 않도록 주의할 것). 그외의 내용은 친절하게 답해주렴!"
 prompt_input = prompt_preset # 수정
 #prompt_input = tab2.text_area("Prompt", value=prompt_preset)
 
